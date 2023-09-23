2b177be90cf6e3a2343e899f8f785821a20bda32
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-09-23 03:02:55 +0300

    Puppet manifest that creates a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# This puppet manifest creates a file and
+# assigns permission and adds content
+
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+  }
